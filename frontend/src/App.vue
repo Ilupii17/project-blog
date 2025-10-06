1ba5d5b02d141abf5c56b07b9afdedf43086a857
ilupii17 <asus07346@outlook.com>
2025-10-06 15:51:16 +0800

    add tailwinds

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="max-w-3xl mx-auto p-6">
+  <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind v3 🚀</h1>
     <h1 class="text-3xl font-bold mb-4">My Blog</h1>
     <PostList :posts="posts" @select="loadPost" />
     <hr class="my-6" />
